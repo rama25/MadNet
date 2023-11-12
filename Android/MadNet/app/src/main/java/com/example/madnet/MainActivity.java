448dbfaf5b10104e7a510bef33945bbe2254b917
Ramapriya R <ramapriya288@gmail.com>
2023-11-11 22:48:23 -0600

    Android

---
+++ Android/MadNet/app/src/main/java/com/example/madnet/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.madnet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button welcomeButton = findViewById(R.id.welcomeButton);
+        welcomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                continueToSecondPage();
+            }
+        });
+    }
+
+    public void continueToSecondPage() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ Android/MadNet/app/src/main/java/com/example/madnet/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.example.madnet;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button pokemonButton = findViewById(R.id.pokemonButton);
+        Button dbzButton = findViewById(R.id.dbzButton);
+
+        // Set click listeners for the buttons
+        pokemonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUrlInBrowser("http://192.168.0.100:5000/1");
+            }
+        });
+
+        dbzButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUrlInBrowser("http://192.168.0.100:5000/3");
+            }
+        });
+    }
+
+    private void openUrlInBrowser(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
+}
